c320b387b95ede80df883a44e063a9eeef356142
Vineet <vineet98love@gmail.com>
2022-08-31 19:23:42 +0530

    init-react-app

---
+++ server.js
@@ -0,0 +1,36 @@
+
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+var app = express();
+
+const PORT  = process.env.PORT || 5000;
+
+
+
+
+
+app.use(express.json());
+
+app.use(cors())
+// For dealing in json
+// app.use(express.urlencoded({ extended: true }));
+
+
+
+
+if(process.env.NODE_ENV=='production'){
+    const path = require('path')
+
+    app.get('/',(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'frontend','build')))
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    })
+}
+
+
+app.listen(PORT, () => {
+    console.log('Server is runnning on', PORT)
+})
+
+module.exports = app;
